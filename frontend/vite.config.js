88dcb11647a52b54b4a9e8fe85b7c67afdc68935
Ansari <ansari_official@yahoo.com>
2023-11-27 20:24:32 +0530

    introducing blogs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,14 @@
+import { resolve } from "path";
+
 module.exports = {
   build: {
     outDir: "build",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        blogs: resolve(__dirname, "blogs/index.html"),
+        about: resolve(__dirname, "about/index.html"),
+      },
+    },
   },
 };
